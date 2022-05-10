c9d2106ac44b6a66e5f4d8ac0203b90359f8d366
RaresZmf <zamfira.rares@gmail.com>
2022-05-11 01:52:49 +0300

    Interfata Finala

--- SSGtemp.py
+++ SSGtemp.py
@@ -4,7 +4,6 @@ import tkinter as tk
 import configparser
 import datetime
 import webbrowser
-from PIL import ImageTk, Image
 
 startWindow = tk.Tk()
 startWindow['bg']='#333333'
@@ -644,11 +643,13 @@ def enduranceWindow():
     exit = tk.Button(bpWrap, text="Exit", command=endWindow.destroy, width=8, height=2, bg="#424242", fg="#d1d1d1")
     exit.pack(expand=True, side=tk.LEFT, padx=5)
 
+canvas = tk.Canvas(startWindow, width = 300, height = 150, bg="#333333", highlightbackground="#333333")      
+canvas.pack(pady=(60,0))      
+img = tk.PhotoImage(file="SSG.png")      
+canvas.create_image(150,75, image=img)    
 
-photo = tk.Frame(startWindow, width=300, height=150, bg="#00FF00")
-photo.pack(side=tk.TOP, pady=30)
-
-endurance = tk.Button(startWindow, text="ENDURANCE", height=7, width=25, bg="#2e2e2e", fg="#b5b5b5", command=enduranceWindow)
+endurance = tk.Button(startWindow, text="START", height=2, width=15, bg="#2e2e2e", fg="#b5b5b5", command=enduranceWindow)
+endurance.config(font=("Helvetical bold",15))
 endurance.pack(expand=True)
 
 startWindow.mainloop()
