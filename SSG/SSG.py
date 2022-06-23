1a0ef0220da920b373d76f09144c0414835b3b63
RaresZmf <zamfira.rares@gmail.com>
2022-06-23 17:33:06 +0300

    File System Remake

---
+++ SSG/SSG.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+import configparser
+from endurance import enduranceFunc
+from sprint import sprintFunc
+
+#colors & fonts 
+
+design = configparser.ConfigParser()
+design.read("design.ini")
+
+background=design.get("COLOR", "background")
+foreground=design.get("COLOR", "foreground")
+accent=design.get("COLOR", "accent")
+textcolor=design.get("COLOR", "textcolor")
+fontType=design.get("FONT", "fontFamily")
+
+
+#V2.1 Color update
+
+startWindow = tk.Tk()
+startWindow['bg']=background
+startWindow.title("SSG+")
+startWindow.geometry("500x400+500+200")
+
+def sprintCommands():
+    sprintFunc()
+    startWindow.destroy()
+
+def enduranceCommands():
+    enduranceFunc()
+    startWindow.destroy()
+
+canvas = tk.Canvas(startWindow, width = 300, height = 150, bg=background, highlightbackground=background)      
+canvas.pack(pady=(60,0))      
+img = tk.PhotoImage(file="SSG.png")      
+canvas.create_image(150,75, image=img) 
+
+endurance = tk.Button(startWindow, text="Endurance", height=2, width=15, bg=foreground, fg="#cccccc", activebackground=accent, command=enduranceCommands)
+endurance.config(font=(fontType,15))
+endurance.pack(expand=True)
+
+sprint = tk.Button(startWindow, text="Sprint", height=2, width=15, bg=foreground, fg="#cccccc", activebackground=accent, command=sprintCommands)
+sprint.config(font=(fontType, 15))
+sprint.pack(expand=True)
+
+startWindow.mainloop()
